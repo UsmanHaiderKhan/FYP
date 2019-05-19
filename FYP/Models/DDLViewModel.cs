193f9b7c0d0766a1bfb0498e85d5d83617b967da
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-05-19 15:04:21 +0500

    Update Added Model Helper Classes

---
+++ FYP/Models/DDLViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FYPManagementLibrary.UsersManagement;
+using System.Collections.Generic;
+
+namespace FYP.Models
+{
+    public class DDLViewModel
+    {
+        public IEnumerable<Department> Departments { get; set; }
+    }
+}
---
+++ FYP/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "The Register field is required")]
+        [Display(Name = "Register-Id")]
+
+        public string RegisterId { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
